0f2e633a9db1f6c7327f34abf854f8c1cc76a8ac
Joshua Zapata <joshuazap@gmail.com>
2015-12-16 16:56:27 -0800

    Tweak application layouts

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
